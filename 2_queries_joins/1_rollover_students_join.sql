1a3adb88c2f6eabab7896654f5b34b54511557ca
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-06-02 19:25:00 -0700

    Additional queries using JOIN, involving the use of Multiple Join statements

--- 2_queries_joins/1_rollover_students_join.sql
+++ 2_queries_joins/1_rollover_students_join.sql
@@ -1,3 +1,4 @@
+/*Rollover Students using Join */
 SELECT students.name as student_name, cohorts.name as cohort_name, cohorts.start_date as cohorts_start_date, students.start_date as students_start_date
 FROM students FULL OUTER JOIN cohorts ON students.cohort_id = cohorts.id
 WHERE cohorts.start_date != students.start_date
